3431c5b9067b93e040b6fa16add5a7f0647a54e0
Cavendish <jokerliyuan87@gmail.com>
2015-06-28 22:26:02 +0800

    init

---
+++ app/src/main/java/com/dagna/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,36 @@
+package com.dagna.android.criminalintent;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    public CrimeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+
+
+}
